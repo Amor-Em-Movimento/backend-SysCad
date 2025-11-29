67d507343b8872707e23a4fb898b37aded07c95b
Gatasso <galassoworkstation@gmail.com>
2025-11-29 20:58:26 -0300

    chore: first commit

---
+++ src/main/java/aem_ong_dev/SyscadAemApplication.java
@@ -0,0 +1,13 @@
+package aem_ong_dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyscadAemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyscadAemApplication.class, args);
+	}
+
+}
